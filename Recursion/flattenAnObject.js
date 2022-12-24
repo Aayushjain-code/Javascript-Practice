30da65c523fdab97cd8b280761650bd74e7ab158
Aayush Jain <aayushjain6020@gmail.com>
2022-12-24 13:26:59 +0530

    dlatten object

---
+++ Recursion/flattenAnObject.js
@@ -0,0 +1,39 @@
+//Flatten An Object
+const user = {
+  name: "Akshay Saini",
+  address: {
+    personal: {
+      city: "Dehradun",
+      state: "Uttrakhand",
+      area: "Majra",
+    },
+    office: {
+      city: "Hyderabad",
+      area: {
+        landmark: "Hi Tech",
+      },
+    },
+    offices: ["a", "b"],
+  },
+};
+
+const flattenObject = (sourceObject, targetObject, key) => {
+  return Object.keys(sourceObject).reduce((finalObject, currentKey) => {
+    if (typeof sourceObject[currentKey] !== "object") {
+      return {
+        ...finalObject,
+        [`${key}_${currentKey}`]: sourceObject[currentKey],
+      };
+    }
+    return {
+      ...finalObject,
+      ...flattenObject(
+        sourceObject[currentKey],
+        finalObject,
+        `${key}_${currentKey}`
+      ),
+    };
+  }, targetObject);
+};
+
+console.log(flattenObject(user, {}, "user"));
